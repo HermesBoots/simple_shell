dbeb255cba51b653534cac1068e3aacad6abb0ac
Sam Hermes <701@holbertonschool.com>
2019-04-06 20:35:39 +0000

    Add PATH, text of current line, and current command name to globals list

--- common.h
+++ common.h
@@ -20,12 +20,18 @@ typedef struct SubString
 /**
  * struct ShellGlobals - stores state of the shell that many functions need
  * @self: the original argv[0]
+ * @command: the name of / path to the command being executed
+ * @line: the text of the line being executed
+ * @path: the current executable search path
  * @line_num: which input line is being executed
  * @interactive: whether this shell is in interactive mode
  */
 struct ShellGlobals
 {
 	SubString self;
+	char const *command;
+	char const *line;
+	char const *path;
 	uint32_t line_num;
 	char interactive;
 } globals;
